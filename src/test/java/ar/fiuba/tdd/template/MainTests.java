6f7a54d358431ee84f9620e9caab934563d2978f
lanibal <larillo@yahoo.com>
2016-09-01 02:36:50 -0300

    Agregados tests unitarios.

--- src/test/java/ar/fiuba/tdd/template/MainTests.java
+++ src/test/java/ar/fiuba/tdd/template/MainTests.java
@@ -2,12 +2,22 @@ package ar.fiuba.tdd.template;
 
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.assertEquals;
 
 public class MainTests {
 
-    @Test
-    public void dummy() {
-        assertEquals(0, 0);
+    private LinkedQueue<Integer> que = new LinkedQueue<>();
+
+    @Test(expected = NoSuchElementException.class)
+    public void emptyQueueRemove() {
+        que.remove();
     }
+
+    @Test(expected = NoSuchElementException.class)
+    public void emptyQueueTop() {
+        que.top();
+    }
+
 }
